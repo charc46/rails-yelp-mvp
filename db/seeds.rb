4af0be10fcc3ef1260f30b912fd848421b5545b9
Charles Crane <charlie.crane46@gmail.com>
2020-11-11 12:08:17 +0000

    Add routes and restaurants controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+6.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    category: categories.sample,
+    address: Faker::Address.street_address
+    )
+end
